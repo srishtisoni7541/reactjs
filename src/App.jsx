5b179f2a28ccc6bdd939d9a5cb5bfa5ffd7ba859
srishti soni <srishtisoni7541@gmail.com>
2024-09-15 23:22:13 +0530

    add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Srishti Soni</h1>
+      <h1 className='text-5xl font-bold'>Srishti Soni</h1>
+      <h2>{import.meta.env.VITE_SECRET_KEY}</h2>
+
     </div>
   );
 };
