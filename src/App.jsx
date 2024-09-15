9b8ec60784509abf65aabbe134a33c7a6608f4d6
srishti soni <srishtisoni7541@gmail.com>
2024-09-15 23:27:58 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     <div>
       <h1 className='text-5xl font-bold'>Srishti Soni</h1>
       <h2>{import.meta.env.VITE_SECRET_KEY}</h2>
+      <button className='p-2 bg-blue-500 rounded-md'>submit</button>
 
     </div>
   );
